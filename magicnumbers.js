811f54217ac3364c0cae04a525f4a3cd538ba3a0
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-14 03:52:51 -0700

    Create magicnumbers.js

---
+++ magicnumbers.js
@@ -0,0 +1,16 @@
+let a =33+11; //first case
+
+
+function comparison(password){
+    if(password.length < 7){ //it doesn't work correctly here
+
+    }
+}
+
+/*
+let maxlength = 7;
+function comparison(password){
+    if(password.length < maxlength){
+
+    }
+}*/
