f8c7f028d9212ba47020fd998b65c90fae9fe95d
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-14 05:58:39 -0700

    Create NestedConditionals.js

---
+++ NestedConditionals.js
@@ -0,0 +1,13 @@
+let booksCount = 19;
+let result;
+
+result = (booksCount > 15) ? 'План на год выполнен!' :
+    (booksCount > 10) ? 'Уже неплохо!' :
+        'Читать и ещё раз читать';
+        
+        
+        
+let firstCheck = false,
+secondCheck = false,    
+ access = firstCheck ? "Доступ запрещен" : secondCheck ? "Доступ запрещен" : "Доступ разрешен";
+      
