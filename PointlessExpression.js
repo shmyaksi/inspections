2e22c1032f2e68bac456eaeb5c5363113695b9b8
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-15 04:28:19 -0700

    Update PointlessExpression.js

--- PointlessExpression.js
+++ PointlessExpression.js
@@ -2,3 +2,25 @@ let a = 0, b=123, c=41;
 let sum = b+a; // this inspection doesn't work here
 let a = 1, b=123, c=41;
 let sum = b*a;
+
+
+function checking(){ //doesn't work
+    let  a = 1,b=123
+    return b * a
+}
+
+function checking1(){
+   return checking()
+}
+checking1()
+
+
+
+for(let i=0; i<10;i++) { //as well
+    let a = 1;
+    if (a > 0) {
+        a--;
+        let sum = a * 5;
+        console.log(sum);
+    }
+}
