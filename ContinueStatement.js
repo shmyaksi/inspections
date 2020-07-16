e1f0f699bd343b7314af9e44063599ac7c599416
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-16 01:10:51 -0700

    Create ContinueStatement.js

---
+++ ContinueStatement.js
@@ -0,0 +1,31 @@
+let text;
+for (let i = 0; i < 10; i++) {
+    if (i !== 3) {
+        continue;
+    }
+    text += "The number is " + i + "<br>";
+}
+
+
+
+let textt = "";
+let j;
+for (j = 0;j < cars.length; j++) {
+    if (cars[j] === "Saab") {
+        continue;
+    }
+    textt += cars[j] + "<br>";
+}
+
+let k = 0;
+let n = 0;
+
+while (k < 5) {
+    k++;
+
+    if (k === 3) {
+        continue;
+    }
+
+    n += k;
+}
