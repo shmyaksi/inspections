55b649cc238d6cd0230f4ad3fb4dc816e597b294
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-16 07:10:57 -0700

    Update ContinueStatement.js

--- ContinueStatement.js
+++ ContinueStatement.js
@@ -1,44 +1,92 @@
-let text;
-for (let i = 0; i < 10; i++) {
-    if (i !== 3) {
-        continue;
-    }
-    text += "The number is " + i + "<br>";
-}
-
-
-
-let textt = "";
-let j;
-for (j = 0;j < cars.length; j++) {
-    if (cars[j] === "Saab") {
-        continue;
-    }
-    textt += cars[j] + "<br>";
-}
-
-let k = 0;
-let n = 0;
-
-while (k < 5) {
-    k++;
-
-    if (k === 3) {
-        continue;
-    }
-
-    n += k;
-}
-
-
-let str = '';  //continue statement with label
-
-loop1:
-    for (let i = 0; i < 5; i++) {
-        if (i === 1) {
-            continue loop1;
+function one() {
+    let text;
+    for (let i = 0; i < 10; i++) {
+        if (i !== 3) {
+            continue;
         }
-        str = str + i;
+        text += "The number is " + i + "<br>";
     }
+}
 
-console.log(str);
+one()
+
+
+
+function two() {
+    let textt = "";
+    let j;
+    cars = ['Saab','Volvo','Subaru'];
+    for (j = 0; j < cars.length; j++) {
+        if (cars[j] === "Saab") {
+            continue;
+        }
+        textt += cars[j] ;
+    }
+    console.log(textt)
+}
+two()
+
+
+
+
+class Car {
+    constructor(brand) {
+        this.carname = brand;
+    }
+    present() {
+        return "I have a " + this.carname;
+    }
+}
+mycar = new Car("Ford");
+
+function asdf() {
+    let textt = "";
+    let j;
+    cars = ['Saab','Volvo','Subaru'];
+    for (j = 0; j < cars.length; j++) {
+        if (cars[j] === "Saab") {
+            continue;
+        }
+        textt += cars[j] ;
+    }
+    console.log(mycar.present())
+}
+
+
+asdf()
+
+
+
+function three() {
+    let k = 0;
+    let n = 0;
+
+    while (k < 5) {
+        k++;
+
+        if (k === 3) {
+            continue;
+        }
+
+        n += k;
+    }
+    console.log(n);
+}
+
+three()
+
+
+function four() { //continue statement with label
+    let str = '';
+
+    loop1:
+        for (let i = 0; i < 5; i++) {
+            if (i === 1) {
+                continue loop1;
+            }
+            str = str + i;
+        }
+
+    console.log(str);
+}
+four()
