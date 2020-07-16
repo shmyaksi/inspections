291c576f4e8650dc8db7605f4eb5fa38d50890f6
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-16 00:41:46 -0700

    Update NegatedConditionals.js

--- NegatedConditionals.js
+++ NegatedConditionals.js
@@ -1,2 +1,13 @@
 if (!a) { m = b } else { m = c }
 let m = !a ? b : c
+
+
+let user;
+    function abcd(){
+        let bc = 'mother';
+        if (!bc) {
+            let m=b;
+            console.log('access denied');
+        }
+        else m=c;
+    }
