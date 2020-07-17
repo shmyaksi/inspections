147be36e3b23d08381549a1d6fa8e5d4f722d6d7
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-17 03:11:29 -0700

    Update NestedConditionals.js

--- NestedConditionals.js
+++ NestedConditionals.js
@@ -1,20 +1,28 @@
-let booksCount = 19;
-let result;
 
-result = (booksCount > 15) ? 'План на год выполнен!' :
-    (booksCount > 10) ? 'Уже неплохо!' :
-        'Читать и ещё раз читать';
+function book() {
+    let booksCount = 19;
+    let result;
+
+    result = (booksCount > 15) ? 'План на год выполнен!' :
+        (booksCount > 10) ? 'Уже неплохо!' :
+            'Читать и ещё раз читать';
+}
+
+book()
         
         
-        
-let firstCheck = false,
-secondCheck = false,    
- access = firstCheck ? "Доступ запрещен" : secondCheck ? "Доступ запрещен" : "Доступ разрешен";
+
+function check() {
+    let firstCheck = false,
+        secondCheck = false,
+        access = firstCheck ? "Доступ запрещен" : secondCheck ? "Доступ запрещен" : "Доступ разрешен";
+}
+check()
       
 let m = !a ?b :c ?v
 
 
-
+function book(){
 let bookshit = 20;  //weird, it doesn't appear
 let a=10;
 let res;
@@ -24,3 +32,20 @@ if (a>=bookshit)
     if ((a>bookshit-5)&&(a<bookshit))
         console.log('keep reading!')
     else console.log('you would better read more');
+}
+book()
+
+
+
+ let user;
+    function abcd(){
+        let bc = 'mother';
+        if (!bc) {
+            console.log('access denied');
+
+        } else {
+            m = '';
+
+        }
+    }
+    abcd()
