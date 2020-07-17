39283cd6baedb7841bd1db119661a30fb4a4d167
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-17 13:04:14 -0700

    Update EmptyBody.js

--- EmptyBody.js
+++ EmptyBody.js
@@ -9,3 +9,24 @@ function three(a,b) {
 three(6,5)
 
 
+let i;
+class books {
+ constructor(name, author, year) {
+  this.name = name;
+  this.author = author;
+  this.year = year;
+
+  while (i < 5) {
+   
+  }
+ }
+  present()
+  {
+   return 'my favourite book is ' + this.name;
+  }
+
+}
+ book1 = new books('love','smith','2005');
+console.log(book1.present());
+
+
