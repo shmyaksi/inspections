73e12eeb0772e59d1755c169ae660283d2f8156e
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-17 02:40:54 -0700

    Update NestedFunction.js

--- NestedFunction.js
+++ NestedFunction.js
@@ -4,10 +4,20 @@ function add() {
     plus();
     return counter;
 }
-
+add()
 
 function a(x) {
     const b = y => x + y;
     return b;
 }
 console.log(a(3)(4));
+
+
+function one(){
+    function two(){
+        console.log( "hello");
+
+    }
+    two()
+}
+one()
