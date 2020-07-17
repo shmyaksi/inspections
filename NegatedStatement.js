0b781b7c4477e436db3eba0f0728a19a855a3af4
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-17 03:53:35 -0700

    Update NegatedStatement.js

--- NegatedStatement.js
+++ NegatedStatement.js
@@ -1,19 +1,39 @@
 function comparison(password){
 
-    if (password.length !=0){
-
+    if (password.length !== 0) {
+        return
     }
     else {
 
-
     }
 }
+comparison(3)
 
-function numeric(adda){ //flipping if-else
-    else {
-        
+
+
+function numeric(adda){
+
+    if (adda !== 0) {
+        return;
+    } else {
+        adda += 1
     }
-    if (adda === 0) {
-        
-    }
+    console.log(adda)
+
+
 }
+
+numeric(5)
+
+
+function str(){
+    let name = 'anna'
+    if(!name){
+        name='another'
+    }
+    else return
+
+}
+str()
+
+
