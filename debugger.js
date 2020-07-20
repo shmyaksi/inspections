4dabd2c6caf35a732c2448bc796ff9516dbefe41
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-20 12:11:34 +0300

    Create debugger.js

---
+++ debugger.js
@@ -0,0 +1,42 @@
+function potentiallyBuggyCode() {
+    debugger;
+    
+}
+
+potentiallyBuggyCode()
+
+
+
+
+function summ(){
+
+    let a = 5, b = 0;
+
+    debugger;
+    return  a+b;
+}
+
+console.log(summ())
+
+
+
+
+class flowers{
+    constructor(flower) {
+
+        this.flower = flower;
+    }
+
+
+   greet(){
+
+        return 'give me ,'+this.flower+', please';
+        debugger;
+   }
+
+
+}
+
+sunflower = new flowers('sunflower')
+
+console.log(sunflower.greet())
