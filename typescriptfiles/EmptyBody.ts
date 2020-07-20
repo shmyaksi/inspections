756b6aeae93967c5c0afe086c1654dfed21ee805
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-20 23:16:31 +0300

    Create EmptyBody.ts

---
+++ typescriptfiles/EmptyBody.ts
@@ -0,0 +1,42 @@
+function three(a,b) {
+    if (a <= b) {
+    }
+    else {
+        a--;
+    }
+    console.log(a,b)
+}
+three(6,5)
+
+
+let i;
+class books {
+    name;author;year:string;
+    constructor(theName, theAuthor, theYear) {
+        this.name = theName;
+        this.author = theAuthor;
+        this.year = theYear;
+
+        while (i < 5) {
+
+        }
+    }
+    present()
+    {
+        return 'my favourite book is ' + this.name;
+    }
+
+}
+let book1 = new books('love','smith','2005');
+console.log(book1.present());
+
+
+let foo={
+    name: 'kate',
+    job:'musician',
+    planet: 'russia'
+}
+
+if (foo.name ==='kate'){
+
+}
