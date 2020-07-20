7b61b7e40e41c9ab859ef72cfb72408aaf308500
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-20 10:26:08 +0300

    Update NegatedStatement.js

--- NegatedStatement.js
+++ NegatedStatement.js
@@ -37,3 +37,32 @@ function str(){
 str()
 
 
+let Person = (function () {
+    let ageKey = Symbol();
+
+    class Person {
+
+        constructor(name) {
+            this.name = name; 
+            this[ageKey] = 20; 
+        }
+
+
+
+        comparison(){
+            if (!this.name) return 'go away'
+            else return 'hello';
+
+        }
+
+        greet() {
+          
+            console.log(`name: ${this.name}, age: ${this[ageKey]}`);
+        }
+    }
+
+    return Person;
+})();
+
+let joe = new Person('Joe');
+joe.greet();
