29efa5c4502807f5b14b1f4a4f85230bc3e8e06f
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-20 11:10:23 +0300

    Update caller.js

--- caller.js
+++ caller.js
@@ -1,12 +1,15 @@
- function xe() {
-        console.log(xe.caller.name)
-    }
-    
-    
-    function myFunc() {
+function xe() {
+    console.log(xe.caller)
+}
+xe()
+
+
+function myFunc() {
     if (myFunc.caller == null) {
         return 'Эта функция была вызвана из верхнего уровня!';
     } else {
         return 'Эта функция была вызвана из ' + myFunc.caller;
     }
 }
+
+console.log(myFunc())
