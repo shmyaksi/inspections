79f5e0c955080e134672a753d24d58ba4880a666
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-20 23:24:22 +0300

    Create BreakStatement.ts

---
+++ typescriptfiles/BreakStatement.ts
@@ -0,0 +1,65 @@
+function one() {
+    let text;
+    for (let i = 0; i < 10; i++) {
+        if (i === 3) {
+            break;
+        }
+        text += "The number is  " + i + " .";
+    }
+    console.log(text);
+}
+
+one()
+
+function two() {
+    let textt;
+    let cars = ["BMW ", "Volvo", "Saab", "Ford"];
+    list: {
+        textt += cars[0] + '';
+        textt += cars[1] + '';
+        break list;
+        textt += cars[2] + '';
+        textt += cars[3] + '';
+    }
+    console.log(textt);
+}
+
+two()
+
+
+function three() {
+
+    let str = '';
+    loop1:
+        for (let ii = 0; ii < 5; ii++) {
+
+            if (ii === 1) {
+                break loop1;
+            }
+            str = str + ii;
+
+        }
+
+    console.log(str);
+}
+
+
+three()
+
+
+function four() {
+    let k, j;
+
+    loop1:
+        for (k = 0; k < 3; k++) {
+            loop2:
+                for (j = 0; j < 3; j++) {
+                    if (k === 1 && j === 1) {
+                        break loop1;
+                    }
+                    console.log('k = ' + k + ', j = ' + j);
+                }
+        }
+}
+
+four()
