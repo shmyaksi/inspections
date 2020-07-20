8d08a2da92020adefd54b689ba97743d053a5027
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-20 13:06:57 +0300

    Create LabeledStatement.js

---
+++ LabeledStatement.js
@@ -0,0 +1,54 @@
+
+function one(){
+even:
+for (let i = 1; i <= 10; i++){
+  console.log(i);
+}
+
+
+
+}
+one()
+
+
+
+
+function one() {
+    labelmark:
+        for (let x = 0; x < 6; x++) {
+
+            if (x == 3) {
+                break labelmark;
+            }
+
+            else return 'continue'
+        }
+}
+
+console.log(one())
+
+
+
+
+let k=0;
+
+function loops(){
+    loop1:
+
+    while(k<6) {
+        k+=1
+        console.log(k)
+
+    }
+
+    loop1:
+
+    for(let i=0;i<10;i++){
+
+        console.log(i)
+    }
+
+
+
+}
+loops()
