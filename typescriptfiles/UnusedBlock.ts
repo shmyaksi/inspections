2b42f250573a7fe57562536b6e7c45f7cce9e6fb
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-20 17:25:21 +0300

    Create UnusedBlock.ts

---
+++ typescriptfiles/UnusedBlock.ts
@@ -0,0 +1,22 @@
+function one(assan) {
+    {
+        let assan = +1;
+    }
+    console.log(assan)
+
+
+}
+
+one(3)
+
+
+class goods {
+    a: string
+
+    constructor(a: string) {
+        {
+            this.a = a;
+        }
+
+    }
+}
