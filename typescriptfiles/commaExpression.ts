4274fc7a26b7d97893154d5f8e60a3ec78365fed
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-20 23:22:59 +0300

    Create commaExpression.ts

---
+++ typescriptfiles/commaExpression.ts
@@ -0,0 +1,43 @@
+let x = 1;
+
+x = (x++, x);
+
+console.log(x);
+
+
+//let y = (2, 3);
+
+console.log(x);
+
+
+let a, b, c;
+
+a = b = 3, c = 4;
+console.log(a);
+
+let x1, y1, z1;
+
+x1 = (y1 = 5, z1 = 6);
+console.log(x1); // 6
+
+
+function one() {
+    let arr = [4, 0, 2, 5];
+    arr[0]=5,3;
+
+    console.log(arr)
+}
+one()
+
+let m = !a ?b :c //conditional expression
+
+function Calc() {
+    let booksCount = 19;
+    let result;
+
+    result = (booksCount > 15) ? 'План на год выполнен!' :
+        (booksCount > 10) ? 'Уже неплохо!' :
+            'Читать и ещё раз читать';
+    console.log(result)
+}
+Calc()
