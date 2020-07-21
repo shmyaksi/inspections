1b6dcb41f409b6bbcb77665109fae9f11dd6fc45
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-21 11:08:37 +0300

    Create DuplicateCheck.ts

---
+++ typescriptfiles/DuplicateCheck.ts
@@ -0,0 +1,21 @@
+function padLeft(value: string, padding: string | number | string) {
+
+    return
+
+}
+
+padLeft('wall', 45);
+
+function extend(p: string): string & string & any {
+
+    return
+}
+
+extend('hello');
+
+function one(x: number): number & number {
+
+    return x * x
+}
+
+console.log(one(2))
