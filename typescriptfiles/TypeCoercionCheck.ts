e5842092d3637552134e9f12d28d231ed34b031c
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-21 11:22:04 +0300

    Create TypeCoercionCheck.ts

---
+++ typescriptfiles/TypeCoercionCheck.ts
@@ -0,0 +1,20 @@
+function one(x: number) {
+    if (x != 5) return `ok`;
+
+
+}
+
+console.log(one(6))
+
+
+function two(y: number) {
+    if (y == null) return 'not ok'
+}
+
+two(4)
+
+function three(c: number) {
+    if (c == 2) return
+}
+
+three(0)
