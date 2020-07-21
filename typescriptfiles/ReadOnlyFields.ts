e19cfbbd0a3c70e2f34be1fcdf0a95b141ffa42d
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-21 12:17:26 +0300

    Create ReadOnlyFields.ts

---
+++ typescriptfiles/ReadOnlyFields.ts
@@ -0,0 +1,28 @@
+class cars {
+
+    private name: string = 'ford'; //TODO
+
+    constructor(private theName: string) {
+
+
+        this.name = theName;
+    }
+
+}
+
+
+class Animal {
+    private name: string; //TODO
+
+    constructor(theName: string) {
+        this.name = theName;
+    }
+}
+
+
+class Person {
+    private _birthYear: number;  //TODO
+    constructor(birthYear: number) {
+        this._birthYear = birthYear;
+    }
+}
