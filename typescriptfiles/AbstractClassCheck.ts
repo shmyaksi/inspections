7b0164118f641714d6b26676fed605ea9abc5407
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-21 10:52:51 +0300

    Create AbstractClassCheck.ts

---
+++ typescriptfiles/AbstractClassCheck.ts
@@ -0,0 +1,16 @@
+ abstract class Cars
+{
+    theName:string;
+    constructor(name:string)
+    {
+        this.theName =name;
+    }
+}
+
+abstract class Books {
+    protected theName: string;
+
+    constructor(name: string) {
+        this.theName = name;
+    }
+}
