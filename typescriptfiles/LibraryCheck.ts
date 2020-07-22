2deffb34634a5af9a7e167d45261f36434ca73fc
Anya <62640644+shmyaksi@users.noreply.github.com>
2020-07-22 14:52:18 +0300

    Create LibraryCheck.ts

---
+++ typescriptfiles/LibraryCheck.ts
@@ -0,0 +1,8 @@
+let tarr = Uint8Array.of(12, 5, 3);
+console.log(tarr.includes(5)); //
+
+
+let myItems: Array<string>=["blah","ok","jj"];
+let exists = myItems.includes('bla'); //should suggest to add library "dom","es2017"
+console.log(exists)
+
