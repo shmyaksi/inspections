db6a8a00df57c007b3b1b59b0d79206e6137a88e
Anna Nikolskaya <shmyaksi@gmail.com>
2020-07-22 18:27:30 +0300

    add loop

--- typescriptfiles/BreakStatement.ts
+++ typescriptfiles/BreakStatement.ts
@@ -63,3 +63,26 @@ function four() {
 }
 
 four()
+
+
+class cars {
+    name: string
+
+    constructor(theName: string) {
+        this.name = theName
+    }
+
+
+}
+
+let i: number = 1
+while (i <= 10) {
+    if (i % 5 == 0) {
+        console.log("The first multiple of 5  between 1 and 10 is : " + i)
+        break     //exit the loop if the first multiple is found
+    }
+    i++
+}  //outputs 5 and exits the loop
+
+
+
