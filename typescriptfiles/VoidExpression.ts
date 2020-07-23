4ae0bf097816976ccbb7e23f768965ab186f3a1a
Anna Nikolskaya <shmyaksi@gmail.com>
2020-07-23 18:27:22 +0300

    add

--- typescriptfiles/VoidExpression.ts
+++ typescriptfiles/VoidExpression.ts
@@ -1,14 +1,45 @@
-void function one() {
-    console.log('boo!');
+void function onon() {
+    console.log('boo!')
 
-};
+}
+()
 
-function three(){
+function threee(){
     let j=void(0);
 
-    if(j===void(0)){
+    if(j===void(0)){ //suggest to change to 'undefined'
         console.log('error')
     }
 }
 
-three()
+threee()
+
+
+function lala():void{
+
+    //console.log('dsdgsg')
+}
+lala()
+
+
+function doSomething(callback: () => void) { //shouldn't appear
+
+   const a = callback
+
+    a();
+
+}
+
+
+function aNumberCallback(): number {
+    return 2;
+}
+
+
+console.log(doSomething(aNumberCallback))
+
+interface school{
+    teacher:void;
+}
+
+
