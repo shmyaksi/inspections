892914981dc896d832acf28ab129f0a72ddd3477
Anna Nikolskaya <shmyaksi@gmail.com>
2020-07-23 13:25:30 +0300

    a

--- typescriptfiles/LabeledStatement.ts
+++ typescriptfiles/LabeledStatement.ts
@@ -1,10 +1,51 @@
+export{}
 function one(){
-    even:
+    even:                           //unnecessary label
         for (let i = 1; i <= 10; i++){
             console.log(i);
         }
 
 
 
+
 }
 one()
+function two() {
+    let j:number;
+
+    myLabel:  // label is used
+        if (j % 2 == 0) {
+        if (j == 12) {
+            console.log("12");
+            break myLabel;
+        }
+        console.log("Odd number, but not 12");
+    }
+}
+
+two()
+
+function doSomething():any {
+
+}
+
+myLabel: {
+    let x = doSomething();
+
+    if (x > 0) {
+        break myLabel;
+    }
+
+    console.log(x)
+    }
+
+doSomething()
+function three() {
+    myLabell: for (let i = 0; i < 10; i++) {// Compliant
+
+        console.log("Loop");
+        break myLabell;
+    }
+}
+
+three()
