bdd81d71a38e0b5e4a48b9ebcc9bec8695254fe4
Anna Nikolskaya <shmyaksi@gmail.com>
2020-07-23 12:44:02 +0300

    a

--- typescriptfiles/Increment.ts
+++ typescriptfiles/Increment.ts
@@ -1,9 +1,35 @@
-function two() {
+function aaa() {
     let s;
     for (let i = 0; i < 5; i++) {
-        s =++i;
+        s = ++i;
         console.log(s)
     }
 
 }
-two()
+
+aaa()
+let ua = 3, ub = 2, uc = 1
+ua = ++ub + uc--;
+let fo = 0, bar = 0
+fo = bar++ / 4;
+console.log(bar);
+
+
+class AA {
+    quantity: number;
+
+    constructor(qua: number) {
+        this.quantity = qua;
+    }
+
+    ca() {
+        if (this.quantity === 0) {
+
+            return ++this.quantity
+
+        } else return 'good'
+    }
+}
+
+let mynew = new AA(20)
+console.log(mynew.ca())
