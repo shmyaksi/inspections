046238ba31ab687474aab9294f7e0a0c44eb9b2a
Anna Nikolskaya <shmyaksi@gmail.com>
2020-07-24 12:58:09 +0300

    add

--- typescriptfiles/CallerCheck.ts
+++ typescriptfiles/CallerCheck.ts
@@ -1,19 +1,19 @@
-/*export{}
-function xe() {
-    return xe.caller //TODO
+export{}
+/*function xe() {
+    return xe.caller //TODO (should appear inspection but TypeError)
 }
 console.log(xe())*/
 
 
 function myFunc1() {
-    if (myFunc1.caller === null) { //TODO
+    if (myFunc1.caller === null) { //TODO (should appear inspection but TypeError)
         return 'Эта функция была вызвана из верхнего уровня!';
     } else {
         return 'Эта функция была вызвана из ' + myFunc1.caller;
     }
 }
 
-console.log(myFunc1()) //TODO
+console.log(myFunc1()) //TODO (should appear inspection but TypeError)
 
 
 function one(){
@@ -23,7 +23,7 @@ function one(){
 
 
         function three(){
-            return three.caller //TODO (should appear)
+            return three.caller //TODO (should appear inspection but TypeError)
         }
     }
 }
