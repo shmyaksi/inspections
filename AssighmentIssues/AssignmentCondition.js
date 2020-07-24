f85adbd66737be482c73722e5e368033e38e339a
Anna Nikolskaya <shmyaksi@gmail.com>
2020-07-24 18:02:59 +0300

    add

---
+++ AssighmentIssues/AssignmentCondition.js
@@ -0,0 +1,40 @@
+function C() {
+    let c;
+    if (c = 9) return c
+
+}
+
+C()
+
+function D() {
+
+    let d = 3;
+    let v;
+    while (d = 2)
+        break;
+
+
+}
+
+D()
+
+function E() {
+    let v;
+    for (v = 0; v = 6; v++) {
+        break;
+        console.log(v)
+    }
+}
+
+E()
+
+function F() {
+    let f = -2;
+    do
+        f++;
+
+
+    while (f = 0)
+}
+
+F()
