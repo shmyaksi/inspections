16185866d1f5f717420915cf5418cb06d1efbec9
Anna Nikolskaya <shmyaksi@gmail.com>
2020-07-24 18:31:34 +0300

    add

---
+++ AssighmentIssues/itself.js
@@ -0,0 +1,33 @@
+function inn(){
+    let x;
+     x=x;
+
+
+}
+inn()
+
+class People2 {
+
+    constructor(age) {
+        this.age = this.age;
+    }
+
+    ager() {
+
+        return this.age
+
+
+    }
+}
+
+let me = new People2(22)
+console.log(me.ager())
+
+let maxlength=2
+function opp(){
+    let j=3;
+    if (j>maxlength) j=j;
+
+}
+opp()
+
