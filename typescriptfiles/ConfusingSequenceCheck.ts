408b9e0e15b4f5b1a709c02b9503b501ac95326f
Anna Nikolskaya <shmyaksi@gmail.com>
2020-07-24 10:29:15 +0300

    add

---
+++ typescriptfiles/ConfusingSequenceCheck.ts
@@ -0,0 +1,35 @@
+function ad(){
+    let a = 34, b=4;
+    let c:number;
+    c = a+++b;
+    console.log(c)
+}
+ad()
+
+function ad2() {
+    let a = 34, b = 4;
+    let c: number;
+
+    for (let i = 0; i < 5; i++) {
+        {
+            c = a-+-b; // TODO
+            console.log(c)
+        }
+    }
+}
+ad2()
+
+console.log('------------------------------')
+function ad3() {
+    let a = 34, b = 4;
+    let c: number;
+
+    for (let i = 0; i < 5; i++) {
+        {
+            c = a-+ -+b; // TODO
+            console.log(c)
+        }
+    }
+}
+ad3()
+
