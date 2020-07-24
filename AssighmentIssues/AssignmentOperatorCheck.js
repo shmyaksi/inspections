995c46f505b17e68bcf5ff2b82bf9e42ead9eb30
Anna Nikolskaya <shmyaksi@gmail.com>
2020-07-24 16:47:15 +0300

    add

---
+++ AssighmentIssues/AssignmentOperatorCheck.js
@@ -0,0 +1,27 @@
+function one(){
+
+    let a,b;
+    a=a+b;
+    a=a/b;
+    a=a*b;
+    b=b&a;
+
+    return b;
+
+
+}
+one()
+
+const c = 4
+let arr = [2,3,4]
+
+function two(){
+    arr[1]=arr[1]+1
+    arr[0]=c*arr[0]; //TODO  (works only for arr[0]*c)
+console.log(arr[0])
+
+}
+
+two()
+
+
