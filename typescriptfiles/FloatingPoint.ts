8cb0cea5cb68ebe8fbef67a5f70821fb5bef4581
Anna Nikolskaya <shmyaksi@gmail.com>
2020-07-24 11:33:55 +0300

    add

--- typescriptfiles/FloatingPoint.ts
+++ typescriptfiles/FloatingPoint.ts
@@ -1,8 +1,18 @@
 function test() {
-    let a = 0.1, b = 0.2
-   // let x = 0.0000001 * 0.2;
-    let x = parseFloat((a*b).toFixed(2))
+    let a = 2., b = 0.2
+        // let x = 0.0000001 * 0.2;
+   let x = parseFloat((a*b).toFixed(2))
 
     console.log(x);
 }
 test();
+
+function test1() {
+    let a = .097, b = 0.2
+    // let x = 0.0000001 * 0.2;
+    let x = parseFloat((a*b).toFixed(2))
+
+    console.log(x);
+}
+test1();
+
