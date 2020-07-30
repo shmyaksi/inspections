8b4fcf3dcbbc17c43cd0af70b25e7e510eeb8bde
Anna Nikolskaya <shmyaksi@gmail.com>
2020-07-30 20:24:09 +0300

    New files

---
+++ General/DestructuringProperties.js
@@ -0,0 +1,27 @@
+const  {
+    target: {
+
+            value: a,
+            type:b,
+            value: c
+        }
+
+} = {
+    target : {
+        value: 2,
+        type:'first'
+    }
+
+}
+
+let options = {
+    size: {
+        width: 100,
+        height: 200
+    },
+    items: ["Cake", "Donut"],
+    extra: true
+};
+
+
+var {x: {y: a, z: b, y: t}} = {x: {y: 7, z: 8}}
