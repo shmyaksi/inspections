a4a9fda8ec8cac311597f65a29da862a60061752
Anna Nikolskaya <shmyaksi@gmail.com>
2020-07-30 20:01:27 +0300

    New files

---
+++ General/DuplicateDeclaration.js
@@ -0,0 +1,35 @@
+function one()
+{
+    let e = {
+        target: {
+                value: 'abc',
+                value: 'kjdv'
+            },
+            password: {
+                value: 'xyz'
+
+        }
+    }
+    console.log(e)
+
+}
+
+
+function two(a,b,a){
+    let aa = 10;
+     aa = 2; //TODO?
+
+ console.log(aa)
+
+}
+
+
+class Person1 {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.firstName = firstName; //todo?
+        this.lastName = lastName;
+    }
+}
+
+
