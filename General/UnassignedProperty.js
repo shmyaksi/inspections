5a44149c047f616893d0b19607884cda7a221094
Anna Nikolskaya <shmyaksi@gmail.com>
2020-08-05 17:15:52 +0300

    Add new file

---
+++ General/UnassignedProperty.js
@@ -0,0 +1,29 @@
+class s1 {
+    constructor(greet2) {
+        this.greet2 = this.n2 // it doesn't work in methods
+        let n2 = this.n2
+    }
+
+}
+
+
+//WEB-30520
+class A2 {
+    a = this.b; // error, 'b' is initialized later
+    b = this.a; // ok
+    c = this.m; // ok, because 'm' is hoisted
+    q = this.q; // error, self-reference in an initializer
+    s = () => this.s;
+    f = function () {
+        return this.f;
+    }; // a different warning, because this is a 'suspicious this' case (WEB-29877)
+    m() {
+    }
+}
+
+
+let asd ; //todo incorrect quick-fix
+function k3(e){
+   return asd*=e
+}
+k3(3)
