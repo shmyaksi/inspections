3b3a1d8e1a46234cbeb8d06f3e3e7dd582099df0
mariia.morozova@jetbrains.com <mariia.morozova@jetbrains.com>
2020-08-09 21:34:30 +0300

    Add case numerations

--- BitwiseIssues/ShiftOperation.js
+++ BitwiseIssues/ShiftOperation.js
@@ -1,11 +1,14 @@
 //Inspection is ON
-function s(){
 
+//Case1
+function s(){
     let z = 1<<1000000
     console.log(z)
 
 }
 s()
+
+//Case2
 function s1(){
 
     let z = 1<<-14
@@ -14,7 +17,7 @@ function s1(){
 }
 s1()
 
-
+//Case3
 const setBit = (num, position) => {
     let mask = 1 << position
     return num | mask
