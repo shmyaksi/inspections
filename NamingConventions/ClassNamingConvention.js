975bb9e4c5f4affaf0286309ce15a0d2086bbedf
Anna Nikolskaya <shmyaksi@gmail.com>
2020-08-12 10:33:38 +0300

    Add samples for new group

---
+++ NamingConventions/ClassNamingConvention.js
@@ -0,0 +1,52 @@
+// Inspection 'class naming convention' is OFF by default
+
+//Case1
+/**
+ @constructor
+ */
+function Boo(title, author) {
+}
+
+//Case2
+/**
+ @constructor
+ */
+function H123() {
+}
+
+//Case3
+/**
+ @constructor
+ */
+function aajk() {
+
+}
+
+//Case4
+/**
+ @class
+ */
+class Poi {
+    constructor(x, y) {
+    }
+}
+
+//Case5
+/**
+ @class
+ */
+class point {
+
+    constructor(x, y) {
+    }
+}
+
+//Case6
+/**
+ @class
+ */
+class Point23 {
+
+    constructor(x, y) {
+    }
+}
