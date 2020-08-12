e79a4f7bde4f3f75f54aa94d9f9b537ee8fe35e0
Anna Nikolskaya <shmyaksi@gmail.com>
2020-08-12 13:11:16 +0300

    Add samples for new group

---
+++ NamingConventions/FunctionNamingConvention.js
@@ -0,0 +1,29 @@
+//Inspection 'function naming convention' is OFF by default
+
+//Case1
+function hel() {
+
+}
+
+//Case2
+function Holla() {
+
+}
+
+//Case3
+function jojis2() {
+
+}
+
+//Case4
+function qwertyuioplkjhgfdsazxcvbnmqwertyu() {
+
+}
+
+//Case5
+function* gaksl() { //todo , why * isn't marked as error?
+
+}
+
+gaksl()
+
